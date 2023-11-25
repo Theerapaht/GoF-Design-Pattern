a677dcc8e1154120cfcba044c3ad3eb5a65b2310
jump <jump@odds.team>
2023-11-25 11:46:24 +0700

    commit 4

--- code/src/observer/lab/weatherStation.test.ts
+++ code/src/observer/lab/weatherStation.test.ts
@@ -2,23 +2,44 @@ import { WeatherData } from './weatherData';
 import { WeatherStation } from './weatherStation';
 
 describe('[Observer - begin] Weather station measurement changed', () => {
-  it('display should be called when measurement changed', () => {
-    // given
-    const weatherStation = new WeatherStation();
-    const data = new WeatherData(32.0, 0.8, 1.0);
+    it('display should be called when measurement changed', () => {
+        // given
+        const weatherStation = new WeatherStation();
+        const data = new WeatherData(32.0, 0.8, 1.0);
 
-    // when
-    weatherStation.update(data);
+        // when
+        weatherStation.update(data);
 
-    // then
-    expect(weatherStation.updateCurrentConditionsDisplay()).toBe(
-      'Current conditions: 32C degrees and 0.8% humidity'
-    );
-    expect(weatherStation.updateStatisticsDisplay()).toBe(
-      'Avg/Max/Min temperature = 32/0.8/1'
-    );
-    expect(weatherStation.updateForecastDisplay()).toBe(
-      'Forecast: More of the same'
-    );
-  });
+        // then
+        expect(weatherStation.updateCurrentConditionsDisplay()).toBe(
+            'Current conditions: 32C degrees and 0.8% humidity'
+        );
+        expect(weatherStation.updateStatisticsDisplay()).toBe(
+            'Avg/Max/Min temperature = 32/0.8/1'
+        );
+        expect(weatherStation.updateForecastDisplay()).toBe(
+            'Forecast: More of the same'
+        );
+    });
+
+    it('should notify observers', () => {
+         // given
+         const weatherStation = new WeatherStation();
+         const data = new WeatherData(32.0, 0.8, 1.0);
+         const observer1 = {
+            update: jest.fn()
+         }
+         const observer2 = {
+            update: jest.fn()
+         }
+         weatherStation.registerobserver(observer1)
+         weatherStation.registerobserver(observer2)
+
+         //when
+         weatherStation.update(data);
+
+         //then
+         expect(observer1.update).toBeCalledWith(data)
+         expect(observer2.update).toBeCalledWith(data)
+    });
 });
--- code/src/observer/lab/weatherStation.ts
+++ code/src/observer/lab/weatherStation.ts
@@ -1,21 +1,28 @@
 import { WeatherData } from './weatherData';
+import { WeatherObserver } from './weatherObserver';
 
 export class WeatherStation {
-  private data = new WeatherData(0, 0, 0);
+    private data = new WeatherData(0, 0, 0);
+    private observers: WeatherObserver[] = [];
 
-  public update(data: WeatherData): void {
-    this.data = data;
-  }
+    public update(data: WeatherData): void {
+        this.data = data;
+        this.observers.forEach((observer) => observer.update(data));
+    }
 
-  updateCurrentConditionsDisplay(): string {
-    return `Current conditions: ${this.data.temperature}C degrees and ${this.data.humidity}% humidity`;
-  }
+    public registerobserver(observers: WeatherObserver): void {
+        this.observers.push(observers)
+    }
 
-  updateStatisticsDisplay(): string {
-    return `Avg/Max/Min temperature = ${this.data.temperature}/${this.data.humidity}/${this.data.pressure}`;
-  }
+    updateCurrentConditionsDisplay(): string {
+        return `Current conditions: ${this.data.temperature}C degrees and ${this.data.humidity}% humidity`;
+    }
 
-  updateForecastDisplay(): string {
-    return `Forecast: More of the same`;
-  }
+    updateStatisticsDisplay(): string {
+        return `Avg/Max/Min temperature = ${this.data.temperature}/${this.data.humidity}/${this.data.pressure}`;
+    }
+
+    updateForecastDisplay(): string {
+        return `Forecast: More of the same`;
+    }
 }
