ff441ad9e819c3cd69c52d394ed1ee13c3bbc5c6
jump <jump@odds.team>
2023-11-25 14:45:17 +0700

    commit 6

--- code/src/command/lab/controller.test.ts
+++ code/src/command/lab/controller.test.ts
@@ -1,3 +1,21 @@
+import { LightOffCommand, LightOnCommand } from "./command/lightCommand";
+import { Controller } from "./controller";
+import { Light } from "./devices";
+
 describe('[Command - lab] Controller', () => {
-  it('should execute device taht wrapped with command object', () => {});
+    it('should execute device taht wrapped with command object', () => {
+        const controller = new Controller();
+        const light = new Light();
+        light.on = jest.fn();
+        light.off = jest.fn();
+
+        const lightOnCommad = new LightOnCommand(light);
+        const lightOffCommad = new LightOffCommand(light);
+
+        controller.execute(lightOnCommad);
+        expect(light.on).toHaveBeenCalled();
+
+        controller.execute(lightOffCommad);
+        expect(light.off).toHaveBeenCalled();
+    });
 });
